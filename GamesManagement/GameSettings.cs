c3a3be96ff44d54fa27ccc831b1e56b74db9ad9f
Alexander <118567820+Chernikov23@users.noreply.github.com>
2025-10-07 13:30:58 +0300

    Create GameSettings.cs

---
+++ GamesManagement/GameSettings.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GameManagement
+{
+    public static class GameSettings
+    {
+        public static int MaxLevel { get; set; } = 100;
+
+        public static bool IsLevelValid(int level)
+        {
+            return level <= MaxLevel;
+        }
+    }
+}
